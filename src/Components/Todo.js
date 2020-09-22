47e26f1d1aeb96a6d44bd97fd7e0b0dc0e2659b1
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2020-09-22 10:51:10 -0700

    se agrego eliminar y terminado

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,12 +1,26 @@
 import React from "react";
 
-function Todo ({ text, id }) {
+function Todo ({ text, todo, todos, setTodos }) {
 
+    const deleteHandler =()=>{
+        setTodos(todos.filter((el) => el.id !==todo.id));
+    };
+
+    const completeHandlre = () =>{
+        setTodos(todos.map(item =>{
+            if(item.id === todo.id){
+                return{
+                    ...item, completed: !item.completed
+                }
+            }
+            return item;
+        }));
+    }
     return(
         <div className="todo" >
-            <li className="todo-item" >{text}</li>
-            <button className="complete-btn"><i className="fas fa-check"></i></button>
-            <button className="trash-btn"><i className="fas fa-trash"></i></button>
+            <li className={`todo-item ${todo.completed ? "completed":""}`} >{text}</li>
+            <button onClick={completeHandlre} className="complete-btn"><i className="fas fa-check"></i></button>
+            <button onClick={deleteHandler} className="trash-btn"><i className="fas fa-trash"></i></button>
         </div>
     );
 }
